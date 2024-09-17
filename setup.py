56f9ab0208f35e6252e06c154829bf0b99296a71
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2024-09-17 14:05:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     install_requires=['numpy', 'tensorflow', 'hyperopt', 'matplotlib', 'seaborn'],
     description='A Python package for synthesizing deep learning models with automatic optimization.',
-    author='Venom Ayush',
+    author='venombolteop',
     author_email='venombolteop@example.com',
     license='MIT',
     url='https://github.com/venombolteop/deepsynth',
