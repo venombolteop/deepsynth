21d1a0d801f66d5040646ac3e1c33853619c3459
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2024-09-17 18:35:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='deepsynth',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=['numpy', 'tensorflow', 'hyperopt', 'matplotlib', 'seaborn'],
     description='A Python package for synthesizing deep learning models with automatic optimization.',
